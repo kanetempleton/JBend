18f0a47ca6944b7b0bd18035c4a91692e468ea4f
kanetempleton <ktempleton96@yahoo.com>
2022-04-26 17:37:39 -0500

    a ton of CRUD stuff

--- universe/src/com/db/crud/CRUDHandler.java
+++ universe/src/com/db/crud/CRUDHandler.java
@@ -3,21 +3,101 @@ package com.db.crud;
 import com.console.Console;
 import com.db.DatabaseUtility;
 import com.db.ServerQuery;
+import com.db.queries.CreateTableQuery;
+import com.db.queries.DeleteTableQuery;
+import com.db.queries.TableExistsQuery;
+import com.util.Misc;
 
-public class CRUDHandler extends DatabaseUtility implements Runnable {
+import java.util.ArrayList;
 
-    private String table;
+public class CRUDHandler<X extends CRUDObject> extends DatabaseUtility implements Runnable {
 
-    public CRUDHandler(String t) {
-       // table = t;
+    public static final String PRIMARY_KEY_STANDARD_TYPE = "VARCHAR(63)";
+    public static final String PRIMARY_KEY_STANDARD_NAME = "id";
+    public static final String STANDARD_SQL_FIELD_TYPE = "TEXT";
+
+    private String primaryKey; //field name of the identifying column
+    private ArrayList<CRUDObject> objectList; //active objects
+    private String ignoreFields[]; //names of instance variables to ignore when computing database structure
+
+    public CRUDHandler(String t, String p) {
+        setTable(t);
+        primaryKey=p;
+        objectList = new ArrayList<>();
+        ignoreFields = new String[]{"default"};
     }
+
+    /*public String[] syncedFields() {
+        return new
+    }*/
 
     public void start() {
-        //initTable();
+        initTable();
     }
 
-    public void initTable(String name) {
+    public void initTable() {
+        System.out.println("Initializing table "+getTable()+"...");
+        checkTableExistence();
+       // checkTableStructure();
+        System.out.println("Initialization complete!");
+    }
 
+    //TODO: get table structure
+
+    private void checkTableExistence() {
+        System.out.println("Checking table existence for "+getTable()+"...");
+
+        ServerQuery q = new TableExistsQuery(this) {
+            public void finish() {
+                if (this.getReturnValue().equals("true")) {
+                    //table exists, do nothing
+                } else if(this.getReturnValue().equals("false")) {
+                    CreateTableQuery q2 = new CreateTableQuery(this.getUtil());
+                } else {
+                    System.out.println("the return value was null.");
+                }
+            }
+        };
+
+     /*   new ServerQuery(this,"SHOW TABLES LIKE \""+table+"\"") {
+            public void done() {
+                if (this.responseSize()==0) { //table doesn't exist so create it
+                    String query = "CREATE TABLE "+table+"("+primaryKey+" VARCHAR(64) PRIMARY KEY,";
+                    new ServerQuery(this.util(),"CREATE TABLE "+table+"("+primaryKey+" VARCHAR(64) PRIMARY KEY, title TEXT, customerName TEXT, customerPhone TEXT, customerEmail TEXT, info TEXT, status TEXT, dueDate TEXT)") {
+                        public void done() {
+                            System.out.println("Successfully initialized database table: tickets");
+                        }
+                    };
+                } else { //table does exist so check its structure
+                    checkTableStructure();
+                }
+            }
+        };*/
+    }
+    private void checkTableStructure() {
+        System.out.println("Checking table structure for "+getTable()+"...");
+    }
+
+    //delete the database table lmaooooooo
+    public void drop() {
+        System.out.println("DELETING TABLE! "+getTable());
+        new DeleteTableQuery(this);
+    }
+
+    //not too sure about this one!
+    public void load(String identifier) {
+        //select * from table where id='crud_id'
+        //new CRUDObject(this,fields,values)
+        new ServerQuery(this,"SELECT * FROM "+getTable()+" WHERE "+primaryKey+"='"+identifier+"';") {
+            public void done() {
+                if (this.responseSize()==1) { //this should be the case...
+                    CRUDObject x = new CRUDObject((CRUDHandler) this.getUtil());
+                    String[] fields = this.response_getFields();
+                    String[] values = this.response_getValues()[0];
+                    x.setFields(fields,values);
+                }
+            }
+        };
     }
 
 
@@ -26,7 +106,80 @@ public class CRUDHandler extends DatabaseUtility implements Runnable {
     }
 
     public void run() {
-       // Console.output("starting CRUD handler for "+t);
+        Console.output("starting CRUD handler for "+getTable());
         start();
     }
+
+
+    /// methodszzz
+
+
+    // create a new database-sync'd object
+    // should generate a blank-slate object
+    // that is ready to be stored in database
+    // note: args[0] should be the primary key
+    public CRUDObject create(String[] args) {
+        CRUDObject o = new CRUDObject(this,args[0]);
+        // store o in database
+        return o;
+    }
+
+
+    public String queryTableStructure() {
+        return "structure from select";
+    }
+    public String computeTableStructure() {
+        System.out.println("computing table structure!!!");
+        if (classtype==null) {
+            System.out.println("class type has not been assigned for this handler.");
+            return "null";
+        }
+        String[] names = Misc.fieldNames(classtype,ignoreFields);//dummy.fieldNames();
+        String out = "("+PRIMARY_KEY_STANDARD_NAME+" "+PRIMARY_KEY_STANDARD_TYPE+"";
+        for (int i=0; i<names.length; i++) {
+            //if (i==names.length-1) {
+             //   out+=", "+names[i]+" "+getSQLTypeForField(names[i])+")"
+            //} else {
+                out+=", "+names[i]+" "+getSQLTypeForField(names[i])+"";
+            //}
+        }
+        out+=")";
+        return out;
+    }
+    public String getSQLTypeForField(String field) {
+        return STANDARD_SQL_FIELD_TYPE;
+    }
+
+    private Class<X> classtype = null;
+
+    public void assignClass(Class<X> t) {
+        classtype = t;
+        System.out.println("assigned class type : "+classtype.getName());
+    }
+   // public static String
+
+
+    public static void testCRUDHandler() {
+        CRUDHandler taskHandler = new CRUDHandler("tasks","id");
+        taskHandler.start();
+
+        //test creating for database
+
+
+
+        //test loading from database
+
+        //
+    }
+
+    public String[] ignoreFields() {
+        return ignoreFields;
+    }
+    public void setIgnoreFields(String[] ig) {
+        ignoreFields = new String[ig.length];
+        for (int i=0; i<ig.length; i++) {
+            ignoreFields[i] = ig[i];
+        }
+    }
+
 }
--- universe/src/com/db/crud/CRUDObject.java
+++ universe/src/com/db/crud/CRUDObject.java
@@ -1,16 +1,202 @@
 package com.db.crud;
 
+import java.lang.reflect.Field;
+
 public class CRUDObject {
 
-    private String tableName;
+    private CRUDHandler handler;
+
+    private String id;
+    private boolean updated;
 
 
-    public CRUDObject(String t) {
-        tableName = t;
+    public CRUDObject(CRUDHandler H, String id) {
+        handler = H;
+        this.id = id;
+        updated = false;
+    //    sync = new String[]{"key"};
     }
 
+    public CRUDObject(CRUDHandler H) {
+        handler = H;
+        this.id = "null";
+        updated = false;
+     //   sync = new String[]{"key"};
+    }
+
+    private boolean ignore(String field){
+        System.out.println("checking ignore factor for "+field);
+        for (String g: handler.ignoreFields()) {
+            System.out.println("... compared to "+g);
+            if (field.equals(g)) {
+                System.out.println("ignored");
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    // INTERFACE
+
+    // set the instance variables
+    public void setFields(String[] fields, Object[] values) {
+        for (int i=0; i<fields.length; i++) {
+            if (!ignore(fields[i]))
+                this.setFieldValue(fields[i],values[i]);
+        }
+    }
+
+
+    //save the thing into a database
+
+    //INSERT INTO table (new entry)
+    public void store() {
+        // CHECK IF ENTRY EXISTS -> ERROR
+        // CREATE ENTRY
+    }
+
+    // SELECT FROM table (load existing entry)
+    public void load() {
+        // SELECT ID FROM TABLE
+        // setFields()
+    }
+
+    //MODIFY TABLE (update existing entry)
+    public void save() {
+        // CHECK IF ENTRY EXISTS -> STORE
+        // UPDATE TABLE
+    }
+
+    //MODIFY TABLE  (delete this entry from database)
+    public void delete() {
+        // DELETE FROM TABLE
+    }
+
+
+
+    // BACKEND
+
+    public Object getFieldValue(String fieldName) {
+        return getFieldValue(this,fieldName);
+    }
+    public void setFieldValue(String fieldName, Object setValue) {
+        setFieldValue(this,fieldName,setValue);
+    }
+
+    public Object getField(String name) {
+        try {
+            Class c = this.getClass();
+            Field f = c.getDeclaredField(name);
+            return f.get(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+    public static Object getFieldValue(CRUDObject o, String fieldName) {
+        try {
+            Class crudclass = CRUDObject.class;
+            Class curclass = o.getClass();
+            Class superclass = o.getClass().getSuperclass();
+            Field f = crudclass.getDeclaredField(fieldName);
+            f.setAccessible(true);
+            return f.get(o);
+        } catch (NoSuchFieldException e) {
+            try {
+                Field f = o.getClass().getDeclaredField(fieldName);
+                f.setAccessible(true);
+                return f.get(o);
+            } catch (Exception e2) {
+                e2.printStackTrace();
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void setFieldValue(CRUDObject o, String fieldName, Object setValue) {
+        try {
+            Class crudclass = CRUDObject.class;
+            Class curclass = o.getClass();
+            Class superclass = o.getClass().getSuperclass();
+            Field f = curclass.getDeclaredField(fieldName);
+            f.setAccessible(true);
+            f.set(o,setValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String[] fieldNames() {
+        Field[] fields = CRUDObject.class.getDeclaredFields();
+        String b = "";
+        for (Field field : fields) {
+            //gives the names of the fields
+            if (!ignore(field.getName())) {
+                field.setAccessible(true);
+                b += field.getName() + ";;";
+            }
+        }
+        return b.split(";;");
+    }
+
+    public String[] fieldNames_include_ignores() {
+        Field[] fields = CRUDObject.class.getDeclaredFields();
+        String b = "";
+        for (Field field : fields) {
+            //gives the names of the fields
+                field.setAccessible(true);
+                b += field.getName() + ";;";
+        }
+        return b.split(";;");
+    }
 
     public static void testCRUDObject() {
-        CRUDObject o = new CRUDObject("tasks");
+
+      /*  System.out.println("testing from CRUDObject class...");
+        CRUDHandler tasks = new CRUDHandler("tasks","id");
+       // CRUDObject o = new CRUDObject(tasks);
+      //  System.out.println("direct access: "+o.objectName+","+o.objectID);
+      //  o.objectID = 420;
+      //  System.out.println("direct access: "+o.objectName+","+o.objectID);
+        for (String x: o.fieldNames()) {
+            System.out.println(x+": "+o.getFieldValue(x));
+        }
+
+        o.setFieldValue("objectID",777);
+        System.out.println("changed object ID to 777");
+        System.out.println("objectID: "+o.getFieldValue("objectID"));*/
+
+
     }
+
+    /*
+    String propertyName = "foo";
+    yourClass.getClass().getSuperClass().getDeclaredField(propertyName);
+     */
+
+
+    public boolean isUpdated() {return updated;}
+    public void setUpdated(boolean b){updated = b;}
+    public String getKey() {return id;}
+    public void setKey(String k) {id=k;}
+    /*public String[] syncedFields() {return sync;}
+    public void addSyncedFields(String[] fldz) {
+        String[] out = new String[fldz.length+1];
+        int i=0;
+        out[i++] = sync[0];
+        for (String f: fldz) {
+            out[i++]=f;
+        }
+        sync = new String[out.length];
+        for (int j=0; j<out.length; j++)
+            sync[j]=out[j];
+    }*/
 }
