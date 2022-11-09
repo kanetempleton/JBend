0ac5df3043f5c60dfc5a6ed3f0b285555bd0e8e4
Kane Templeton <ktempleton96@yahoo.com>
2022-11-09 02:26:04 -0600

    framework for pagebuilder functions within html code

--- universe/src/com/server/web/pages/PageBuilder.java
+++ universe/src/com/server/web/pages/PageBuilder.java
@@ -87,6 +87,75 @@ public class PageBuilder {
         html += ui;
     }
 
+    private void replaceBuilderFunctions() {
+        String[] lines = ui.split("\n");
+        for (int i=0; i<lines.length; i++) {
+            if (lines[i].startsWith("$[") && lines[i].endsWith("]")) {
+                lines[i] = translateCode(lines[i].substring(2,lines[i].length()-2));
+            }
+        }
+    }
+
+    private String translateCode(String code) {
+        String F = "";
+        String A = "";
+        String function = "";
+        String args = "";
+        boolean open = false;
+        for (int i=0; i<code.length();i++) {
+            if (code.charAt(i) == '(') {
+                open = true;
+            }
+            else if (code.charAt(i) == ')') {
+                open = false;
+                F = F.length() == 0 ? function : F + "," + function;
+                A = A.length() == 0 ? args : A + ","+args;
+                function = "";
+                args = "";
+            }
+            else if (open) {
+                args += code.charAt(i);
+            } else {
+                function += code.charAt(i);
+            }
+        }
+        String out = "";
+        String[] functions = F.split(",").length==0 ? new String[]{F} : F.split(",");
+        String[] arguments = A.split(",").length==0 ? new String[]{A} : A.split(",");
+        if (functions.length != arguments.length) {
+            return "<p> PageBuilder Syntax Error for "+code+" </p>\n";
+        }
+        for (int i=0; i<functions.length; i++) {
+            out += processFunction(functions[i],arguments[i]);
+        }
+        return out;
+    }
+
+    private String processFunction(String f, String args) {
+        return "<p> PageBuilder Placeholder for Function: "+f+"("+args+") </p>\n";
+    }
+
 
 
 }
+
+
+/* SPECIFICATION
+
+    .ui file:
+    - First 3 lines must follow the following format format:
+    Page title text
+        Page description
+        [keyword1, keyword2, keyword3]
+
+    - The 4th line can optionally be [navbar] to include site navigation at the top of page
+    - Each line in the remainder of the file must either be:
+        - html code
+        - page builder function (specified below)
+
+    Page builder functions: $[function1(args11 args12) function2(args21 args22)]
+    - this sequence must be the only text contents of the line it is called in
+    - the final character in the line must be ']' (no spaces or tabs at end)
+    - no commas allowed
+
+ */
